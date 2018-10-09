800e965a01fac2308f785767ab140333055dd656
Jailson Santos <jailson_donald_cobain@hotmail.com>
2018-10-08 22:57:49 -0300

    preparando ambiente SPRINT

---
+++ my-app/src/assets/js/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAd0OiUHhvaU5SSpSII2GuDP55Exvx2dPY",
+    authDomain: "ic-sprint.firebaseapp.com",
+    databaseURL: "https://ic-sprint.firebaseio.com",
+    projectId: "ic-sprint",
+    storageBucket: "ic-sprint.appspot.com",
+    messagingSenderId: "9955807748"
+  };
+  firebase.initializeApp(config);
